e72ffc2800bd4c544b85053d47980e3f387c0b1d
Jonathan Ezeugo <jonathan.ezeugo@gmail.com>
2021-01-29 13:14:38 -0600

    Correction on python file

--- clim_app.py
+++ clim_app.py
@@ -22,8 +22,8 @@ Base.prepare(engine, reflect=True)
 # Save references to each table
 measurement = Base.classes.measurement
 station = Base.classes.station
-# Create our session (link) from Python to the DB
-session = Session(engine)
+# # Create our session (link) from Python to the DB
+# session = Session(engine)
 
 # 2. Create an app
 app = Flask(__name__)
@@ -40,7 +40,7 @@ def Home():
         f"/api/v1.0/tobs<br/>"
         f"/api/v1.0/start<br/>"
         f"/api/v1.0/end<br/>"
-        f"/api/v1.0/<start>/<end>"        
+        f"/api/v1.0/<start>,<end>"        
     )
 
 
@@ -48,6 +48,8 @@ def Home():
 # App route to precipation information for the range of dates in DB
 @app.route("/api/v1.0/precipitation")
 def preciptn():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)    
     previous_yr =dt.date(2017,8,23) - dt.timedelta(days=365)
 
     results = session.query(measurement.date, measurement.prcp).filter(measurement.date >= previous_yr).all()
@@ -60,25 +62,44 @@ def preciptn():
 # App route to generate a JSON list of stations from the dataset
 @app.route("/api/v1.0/stations")
 def stations():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
     # Querying the stations DB for station names which would populate the list
     stns = session.query(station.name).all()
     session.close()
+
+    # Converting list of tuples into normal list
+    all_stns = list(np.ravel(stns))
     return jsonify(stns)
 
 # App route to generate temperature data for the most active station for the last year
 @app.route("/api/v1.0/tobs")
 def temp():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
     year_ago = dt.date(2017,8,23) - dt.timedelta(days=365)
 
     # Perform a query to retrieve the date and precipitation scores
-    date_n_precp = session.query(measurement.date, measurement.tobs).filter(measurement.date >= year_ago).all()
+    hghst_stn = session.query(station.name, measurement.station, func.count(measurement.station).label('count')).\
+        filter(measurement.station == station.station).group_by(measurement.station).order_by(desc('count')).first()
+    stn_name = hghst_stn[0]
+    stn_wID = hghst_stn[1]
 
+    date_n_precp = session.query(measurement.date, measurement.tobs).filter(measurement.date >= year_ago).filter(measurement.station == stn_wID).all()
+    # Closing out session
     session.close()
-    return jsonify(date_n_precp)
+
+    full_stn_data = [{'Date': date, 'Temp': temp} for date, temp in date_n_precp]
+
+    fst_stn = list(np.ravel(full_stn_data))
+     
+    return jsonify(fst_stn)
 
 # App route to generate min temp, avg temp, and max temp for a given start or end date range
 @app.route("/api/v1.0/start")
 def calc_temp():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
     # For start-only analysis
     start_date = '2016-08-23'
     #end_date = '2017-08-23'
@@ -91,30 +112,34 @@ def calc_temp():
     return jsonify(results)
 
 
-@app.route("/api/v1.0/end")
-def end_dt_tmp():
-    
-    end_date = '2017-08-23'
+@app.route("/api/v1.0/<start>")
+def end_dt_tmp(start):
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+    #end_date = '2017-08-23'
     
     # Query for start date and above covering last year before most recent year
-    end_results = session.query(func.min(measurement.tobs), func.avg(measurement.tobs), func.max(measurement.tobs)).\
-        filter(measurement.date <= end_date).all()
+    start_results = session.query(func.min(measurement.tobs), func.avg(measurement.tobs), func.max(measurement.tobs)).\
+        filter(measurement.date >= start).all()
     # Session close to free up memory
     session.close()
-    return jsonify(end_results)
+    return jsonify(start_results)
 
 
-# App route for end date min, avg and max temp for start and end
-@app.route("/api/v1.0/<start>/<end>")
+# 
+@app.route("/api/v1.0/<start>,<end>")
 def start_end_dt(start,end):
-    start = '2016-08-15'
-    end = '2016-12-10'
-    #(start,end)
-    tmp_results = session.query(func.min(measurement.tobs), func.avg(measurement.tobs), func.max(measurement.tobs)).filter(measurement.date >= start).filter(measurement.date <= end).all()
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+    
+    finds = session.query(func.min(measurement.tobs), func.avg(measurement.tobs), func.max(measurement.tobs)).\
+                filter(measurement.date >= start).filter(measurement.date <= end).all()
     session.close()
-    return jsonify(tmp_results)
 
-
+    # Converting list of tuples into normal list
+    all_finds = list(np.ravel(finds))
+    return jsonify(all_finds)
+    
 
 # This section was inputed for when client requests are made but I thought it may be 
 # too broad to tidy up for this HW. It defines end point generation for faulty entries
